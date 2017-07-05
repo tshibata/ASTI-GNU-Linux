6538ac88991cecbbb28dc13fd29979ed80035c67
tshibata <tshibata@boutem.com>
2017-07-05 23:05:03 +0200

    frame buffer

--- 102-linux.sh
+++ 102-linux.sh
@@ -16,4 +16,8 @@ make mrproper
 
 make i386_defconfig
 
+# For frame buffer
+sed -i 's/^# CONFIG_FB_BOOT_VESA_SUPPORT is not set$/CONFIG_FB_BOOT_VESA_SUPPORT=y/' .config
+sed -i 's/^# CONFIG_FB_VESA is not set$/CONFIG_FB_VESA=y/' .config
+
 make INSTALL_HDR_PATH=$HOME/target/usr headers_install
