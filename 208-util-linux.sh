06d2213308df895f7ee81bd9ccad0bd6c82aeedf
tshibata <tshibata@boutem.com>
2017-07-09 21:14:37 +0200

    minimun programs from util-linux

--- 208-util-linux.sh
+++ 208-util-linux.sh
@@ -18,15 +18,25 @@ mkdir build
 
 cd build/
 
+# Install only fsck, kill, mount (including umount), mountpoint and sulogin.
+
 ../configure PKG_CONFIG='' \
              --host=i686-cross-linux-gnu \
-             --disable-makeinstall-chown \
              --prefix=$HOME/target \
              --include=$HOME/target/usr/include \
              --datarootdir=$HOME/target/usr/share \
              --without-python \
              --without-systemd \
-             --disable-shared
+             --disable-shared \
+             --disable-makeinstall-chown \
+             --disable-all-programs \
+             --enable-fsck \
+             --enable-kill \
+             --enable-libblkid \
+             --enable-libmount \
+             --enable-mount \
+             --enable-mountpoint \
+             --enable-sulogin
 
 make
 
