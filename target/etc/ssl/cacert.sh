a3c7d92124ae370ce7311a839572c0b79cc67c4b
tshibata <tshibata@boutem.com>
2017-07-11 02:05:10 +0200

    backup cacert files

--- target/etc/ssl/cacert.sh
+++ target/etc/ssl/cacert.sh
@@ -3,10 +3,15 @@
 set -e
 
 SSLDIR=`dirname $0`
-rm -rf $SSLDIR/certs
+
+test -e $SSLDIR/certs && mv --backup=numbered $SSLDIR/certs $SSLDIR/certs.old
+
 mkdir $SSLDIR/certs
 
+test -e $SSLDIR/ca-bundle.crt && mv --backup=numbered $SSLDIR/ca-bundle.crt $SSLDIR/ca-bundle.crt.old
+
 curl -o $SSLDIR/ca-bundle.crt https://curl.haxx.se/ca/cacert.pem
+
 cat $SSLDIR/ca-bundle.crt | grep -v "^$" | grep -v "^#" | while read LINE
 do
 	echo $LINE >> $SSLDIR/certs/tmp.pem
